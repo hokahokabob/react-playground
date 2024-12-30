61b620925d0884fd55d5dfd36a1c081d76c43c2a
yuuta <>
2024-12-30 15:47:46 +0900

    add feature to draw cards from the deck

--- src/YuGiOhCards.tsx
+++ src/YuGiOhCards.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Suspense, use } from "react";
+import { Suspense, use, useActionState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
 export type YuGiOhCard = {
@@ -19,13 +19,38 @@ export type CardImages = {
   image_url_small: string;
 };
 
-function YuGiOhCards(
-  {cardsPromise}: {cardsPromise: Promise<YuGiOhCard[]>}
-) {
+type DeckCondition = {
+  cardIndex: number;
+  deckSize: number;
+};
+
+function randomIndex(deckSize: number): number {
+  return Math.floor(Math.random() * deckSize);
+}
+
+async function drawCard({ deckSize }: DeckCondition): Promise<DeckCondition> {
+  const newIndex = randomIndex(deckSize);
+  return { cardIndex: newIndex, deckSize } as DeckCondition;
+}
+
+function YuGiOhCards({
+  cardsPromise,
+}: {
+  cardsPromise: Promise<YuGiOhCard[]>;
+}) {
   const cards: YuGiOhCard[] = use(cardsPromise);
+  const deckSize = cards.length;
+  const [deckConditionState, formAction] = useActionState(drawCard, {
+    cardIndex: randomIndex(deckSize),
+    deckSize,
+  });
+
   return (
     <div>
       <h1>Yu-Gi-Oh! Cards</h1>
+      <form>
+        <button formAction={formAction}>Draw Card</button>
+      </form>
       <table>
         <thead>
           <tr>
@@ -41,30 +66,38 @@ function YuGiOhCards(
           </tr>
         </thead>
         <tbody>
-          {cards.map((card) => (
-            <tr key={card.id}>
-              <td>{card.id}</td>
-              <td>{card.name}</td>
-              <td>{card.desc}</td>
-              <td>{card.atk}</td>
-              <td>{card.def}</td>
-              <td>{card.level}</td>
-              <td>{card.race}</td>
-              <td>{card.attribute}</td>
-              <td>
-                <img src={card.card_images[0].image_url_small} alt={card.name} width="200px"/>
-              </td>
-            </tr>
-          ))}
+          {cards
+            .filter((_card, index) => index === deckConditionState.cardIndex)
+            .map((card) => (
+              <tr key={card.id}>
+                <td>{card.id}</td>
+                <td className="font-mono font-semibold">{card.name}</td>
+                <td>{card.desc}</td>
+                <td className="font-mono font-semibold">{card.atk}</td>
+                <td className="font-mono font-semibold">{card.def}</td>
+                <td className="font-mono">{card.level}</td>
+                <td>{card.race}</td>
+                <td>{card.attribute}</td>
+                <td>
+                  <img
+                    src={card.card_images[0].image_url_small}
+                    alt={card.name}
+                    width="200px"
+                  />
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
   );
 }
 
-export default function YuGiOhCardsContainer(
-  {cardsPromise}: {cardsPromise: Promise<YuGiOhCard[]>}
-) {
+export default function YuGiOhCardsContainer({
+  cardsPromise,
+}: {
+  cardsPromise: Promise<YuGiOhCard[]>;
+}) {
   return (
     <ErrorBoundary fallback={<p>⚠️Something went wrong</p>}>
       <Suspense fallback={<div>Loading...</div>}>
