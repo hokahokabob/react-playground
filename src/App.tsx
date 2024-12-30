2615ca7490e7fecd775bee1fd9c14ec387b86312
yuuta <>
2024-12-30 15:48:12 +0900

    add tailwind basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import "./App.css";
+import "./output.css";
 import { fetchCards } from "./web-api";
 import YuGiOhCardsContainer, { YuGiOhCard } from "./YuGiOhCards";
 
@@ -9,11 +10,11 @@ const url =
   "https://db.ygoprodeck.com/api/v7/cardinfo.php?&sort=name&type=Normal%20Monster&race=Dragon,Spellcaster";
 
 export default function App() {
-  const cardsPromiseData: Promise<YuGiOhCard[]> = fetchCards(url);
+  const cardsPromise: Promise<YuGiOhCard[]> = fetchCards(url);
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <YuGiOhCardsContainer cardsPromise={cardsPromiseData} />
+      <YuGiOhCardsContainer cardsPromise={cardsPromise} />
     </Suspense>
   );
 }
