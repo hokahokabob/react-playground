81190341515b8deaeeecf4ab5627a53133efacd4
yuuta <>
2024-12-31 14:31:16 +0900

    Limit the deck balance

--- src/YuGiOhCards.tsx
+++ src/YuGiOhCards.tsx
@@ -22,16 +22,44 @@ export type CardImages = {
 type DeckCondition = {
   cardIndex: number;
   deckSize: number;
+  deckBalance: number; // After drawing certain number of cards, the duel ends
+  shouldStopDrawing: boolean;
 };
 
 function randomIndex(deckSize: number): number {
   return Math.floor(Math.random() * deckSize);
 }
 
-async function drawCard({ deckSize }: DeckCondition): Promise<DeckCondition> {
-  const newIndex = randomIndex(deckSize);
-  return { cardIndex: newIndex, deckSize } as DeckCondition;
+async function drawCard({
+  deckSize,
+  deckBalance,
+}: DeckCondition): Promise<DeckCondition> {
+  if (deckBalance === 0) {
+    return {
+      cardIndex: -1,
+      deckSize,
+      deckBalance: 0,
+      shouldStopDrawing: true,
+    } as DeckCondition;
+  }
+  const newIndex = randomIndex(deckSize); // Find a card randomly
+  const newDeckBalance = deckBalance - 1;
+  return {
+    cardIndex: newIndex,
+    deckSize,
+    deckBalance: newDeckBalance,
+  } as DeckCondition;
 }
+
+const Hanase = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <p className="text-3xl font-bold mb-4">もうやめて！</p>
+      <img src="/ha_na_se.jpg" />
+      <p className="text-3xl font-bold mt-4">HA NA SE</p>
+    </div>
+  );
+};
 
 function YuGiOhCards({
   cardsPromise,
@@ -40,9 +68,12 @@ function YuGiOhCards({
 }) {
   const cards: YuGiOhCard[] = use(cardsPromise);
   const deckSize = cards.length;
+  const defaultDeckBalance = 15;
   const [deckConditionState, formAction] = useActionState(drawCard, {
     cardIndex: randomIndex(deckSize),
     deckSize,
+    deckBalance: defaultDeckBalance,
+    shouldStopDrawing: false,
   });
 
   return (
@@ -56,50 +87,54 @@ function YuGiOhCards({
           Draw Card
         </button>
       </form>
-      <table className="min-w-full bg-white border border-gray-200">
-        <thead>
-          <tr>
-            <th className="py-2 px-4 border-b">ID</th>
-            <th className="py-2 px-4 border-b">Name</th>
-            <th className="py-2 px-4 border-b">Description</th>
-            <th className="py-2 px-4 border-b">ATK</th>
-            <th className="py-2 px-4 border-b">DEF</th>
-            <th className="py-2 px-4 border-b">Level</th>
-            <th className="py-2 px-4 border-b">Race</th>
-            <th className="py-2 px-4 border-b">Attribute</th>
-            <th className="py-2 px-4 border-b">Image</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cards
-            .filter((_card, index) => index === deckConditionState.cardIndex)
-            .map((card) => (
-              <tr key={card.id} className="hover:bg-gray-100">
-                <td className="py-2 px-4 border-b font-mono">{card.id}</td>
-                <td className="py-2 px-4 border-b font-mono font-semibold">
-                  {card.name}
-                </td>
-                <td className="py-2 px-4 border-b">{card.desc}</td>
-                <td className="py-2 px-4 border-b font-mono font-semibold">
-                  {card.atk}
-                </td>
-                <td className="py-2 px-4 border-b font-mono font-semibold">
-                  {card.def}
-                </td>
-                <td className="py-2 px-4 border-b font-mono">{card.level}</td>
-                <td className="py-2 px-4 border-b">{card.race}</td>
-                <td className="py-2 px-4 border-b">{card.attribute}</td>
-                <td className="py-2 px-4 border-b">
-                  <img
-                    src={card.card_images[0].image_url_small}
-                    alt={card.name}
-                    className="min-w-36"
-                  />
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+      {deckConditionState.shouldStopDrawing ? (
+        <Hanase />
+      ) : (
+        <table className="min-w-full bg-white border border-gray-200">
+          <thead>
+            <tr>
+              <th className="py-2 px-4 border-b">ID</th>
+              <th className="py-2 px-4 border-b">Name</th>
+              <th className="py-2 px-4 border-b">Description</th>
+              <th className="py-2 px-4 border-b">ATK</th>
+              <th className="py-2 px-4 border-b">DEF</th>
+              <th className="py-2 px-4 border-b">Level</th>
+              <th className="py-2 px-4 border-b">Race</th>
+              <th className="py-2 px-4 border-b">Attribute</th>
+              <th className="py-2 px-4 border-b">Image</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cards
+              .filter((_card, index) => index === deckConditionState.cardIndex)
+              .map((card) => (
+                <tr key={card.id} className="hover:bg-gray-100">
+                  <td className="py-2 px-4 border-b font-mono">{card.id}</td>
+                  <td className="py-2 px-4 border-b font-mono font-semibold">
+                    {card.name}
+                  </td>
+                  <td className="py-2 px-4 border-b">{card.desc}</td>
+                  <td className="py-2 px-4 border-b font-mono font-semibold">
+                    {card.atk}
+                  </td>
+                  <td className="py-2 px-4 border-b font-mono font-semibold">
+                    {card.def}
+                  </td>
+                  <td className="py-2 px-4 border-b font-mono">{card.level}</td>
+                  <td className="py-2 px-4 border-b">{card.race}</td>
+                  <td className="py-2 px-4 border-b">{card.attribute}</td>
+                  <td className="py-2 px-4 border-b">
+                    <img
+                      src={card.card_images[0].image_url_small}
+                      alt={card.name}
+                      className="min-w-36"
+                    />
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
